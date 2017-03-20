80e902169589d3a234180ca6b13ac7462ea32434
Josh Naylor <8ctopotamus@gmail.com>
2017-03-19 19:10:08 -0500

    Switched RWcalendar for DateTime Picker

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-import Suggestions from './src/components/Suggestions'
+import PlanBrunch from './src/components/PlanBrunch'
 
 export default class App extends React.Component {
   render() {
     return (
-      <Suggestions />
+      <PlanBrunch />
     )
   }
 }
--- src/components/PlanBrunch.js
+++ src/components/PlanBrunch.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react'
-import {
-	Text,
-	TouchableOpacity,
-	View
-} from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 import sharedStyles from '../utils/shared-styles'
 
 class PlanBrunch extends Component {
+	state = {
+    isDateTimePickerVisible: false,
+  };
+
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  _handleDatePicked = (date) => {
+    console.log('A date has been picked: ', date);
+    this._hideDateTimePicker();
+  };
+
 	render() {
 		return (
 			<View style={sharedStyles.container}>
-				<Text style={[sharedStyles.title]}>Have You Tried...</Text>
-
-				<Text>List of places</Text>
-			</View>
+				<Text style={sharedStyles.title}>When da Brunch at?</Text>
+        <TouchableOpacity
+					style={sharedStyles.button}
+					onPress={this._showDateTimePicker}>
+          <Text>Show TimePicker</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+					mode={'datetime'}
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={this._hideDateTimePicker}
+        />
+      </View>
 		)
 	}
 }
